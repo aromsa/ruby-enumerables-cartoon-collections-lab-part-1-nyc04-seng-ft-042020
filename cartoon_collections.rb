d1fb90e89ef36b5c4eddc7cf74b39628142e3b89
anna bateman <annacbateman@gmail.com>
2020-04-10 20:37:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,13 @@ def list_dwarves(array)
     dwarf_hash[dwarf] = index
     "#{index}. #{dwarf.capitalize}"
     
+    hash = Hash.new
+%w(cat dog wombat).each_with_index { |item, index|
+  hash[item] = index
+}
+hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
+    
+    
   end
   #dwarf_hash
 end
