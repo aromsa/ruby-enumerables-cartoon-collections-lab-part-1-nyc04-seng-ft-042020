be36e19c22a8a550d1584f5baeae7e5308088c98
anna bateman <annacbateman@gmail.com>
2020-04-10 20:31:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def list_dwarves(array)
   dwarf_hash = {}
   array.each_with_index do |dwarf, index|
     dwarf_hash[dwarf] = index
-    "#{index}. #{dwarf.capitalize}"
+    dwarf_hash << #{index}. #{dwarf.capitalize}"
     
   end
   dwarf_hash
