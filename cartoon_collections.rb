a4d90655b7cb9376cf5e4f38ff2f81bd9947b2cb
anna bateman <annacbateman@gmail.com>
2020-04-10 19:49:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each do |dwarf|
-    index = dwarf.each_with_index
+    index = dwarf.each_with_index + 1
     
   end
 end
