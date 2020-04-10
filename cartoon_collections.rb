113770d0a85c4d006ea30f17c2a3e960de1c4091
anna bateman <annacbateman@gmail.com>
2020-04-10 20:31:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,10 +24,10 @@ def list_dwarves(array)
   # Print a numbered list of each element
   index = array.each_with_index + 1
   dwarf_hash = {}
-  array.each_with_index do |dwarf, index|
+  array.each do |dwarf, index|
     dwarf_hash[dwarf] = index
     dwarf_hash << #{index}. #{dwarf.capitalize}"
     
   end
-  #dwarf_hash
+  dwarf_hash
 end
