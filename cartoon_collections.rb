30716da534cf7af0a5a8daaedb4a2bac90c51562
anna bateman <annacbateman@gmail.com>
2020-04-10 19:58:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   index = array.each_with_index + 1
-  array.each do |dwarf|
+  array.each_with_index do |dwarf|
     puts "#{index}. #{dwarf.capitalize}"
     
     
